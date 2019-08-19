ed5ab23ba7697a29ad6889dbfdead15c66247fa9
Niek <niek@keersop.nl>
2019-08-19 19:53:40 +0200

    added the cta, cleaned stuff up, and added a default container

--- src/components/layout/Navigation.vue
+++ src/components/layout/Navigation.vue
@@ -1,15 +1,22 @@
 <template>
   <nav class="nav">
-    <div>asdf</div>
-    <div class="nav__right">
-      <ul>
-        <li>Over ons</li>
-        <li>Evenementen</li>
-        <li>Commissies</li>
-        <li>Merchandise</li>
-        <li>Korting</li>
-        <li>Mijn account</li>
-      </ul>
+    <div class="container">
+      <div class="nav__brand">
+        <img src="@/assets/images/logo.png" alt="logo" />
+      </div>
+      <div class="nav__click-to-action">
+        <a class="button">Meld je aan!</a>
+      </div>
+      <div class="nav__right">
+        <ul>
+          <li>Over ons</li>
+          <li>Evenementen</li>
+          <li>Commissies</li>
+          <li>Merchandise</li>
+          <li>Korting</li>
+          <li>Mijn account</li>
+        </ul>
+      </div>
     </div>
   </nav>
 </template>
@@ -28,14 +35,20 @@ export default class Navigation extends Vue {}
   display: flex;
   padding: 15px 150px;
 
-  img.logo {
-    width: 69px;
-    height: 40px;
+  &__brand {
+    img {
+      width: 100px;
+    }
+  }
+
+  &__click-to-action {
+    margin-left: 40px;
+    padding: 10px;
   }
 
   &__right {
-    display: flex;
     margin-left: auto;
+    padding: 2px;
   }
 
   ul {
@@ -49,8 +62,31 @@ export default class Navigation extends Vue {}
 
       color: white;
 
-      padding: 12px;
+      padding: 12px 8px;
     }
   }
+}
+
+// TODO: TEMP, FIX WHEN DEFAULTS ARE MADE
+
+.button {
+  display: block;
+  background: #663265;
+  border-radius: 2px;
+  padding: 12px;
+
+  font-family: Poppins;
+  font-weight: bold;
+  font-size: 12px;
+
+  text-align: center;
+
+  color: #ffffff;
+}
+
+.container {
+  width: 1200px;
+  display: flex;
+  margin: 0 auto;
 }
 </style>
