28518365bb1306bf7c00cb00b63616182552a9d4
Niek <niek@keersop.nl>
2019-08-19 22:24:22 +0200

    completed the footer layout

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,11 +1,32 @@
 <template>
-  <footer>
+  <footer class="footer">
     <div class="container">
-      
+      <div class="footer__information">
+        <span>Copyright 2019 s.v. Salve Mundi. Alle rechten voorbehouden</span>
+        <hr />
+        <div>
+          <span>Rachelsmolen 1</span>
+          <span>info@salvemundi.nl</span>
+        </div>
+        <div>
+          <span>5612 MA Eindhoven</span>
+          <span>KvK nr. 70280606</span>
+        </div>
+      </div>
+      <div class="footer__social">
+        <div>
+          <img src="@/assets/images/logo.png" alt="facebook" />
+        </div>
+        <div>
+          <img src="@/assets/images/logo.png" alt="instagram" />
+        </div>
+        <div>
+          <a>Privacy</a>
+        </div>
+      </div>
     </div>
   </footer>
 </template>
-
 <script>
 import { Component, Vue } from "vue-property-decorator";
 
@@ -13,8 +34,38 @@ import { Component, Vue } from "vue-property-decorator";
 export default class Footer extends Vue {}
 </script>
 <style lang="scss">
-footer {
-  height: 112px;
+.footer {
+  margin-top: 50px;
   background: #663265;
+  padding: 22px 0;
+  color: white;
+
+  hr {
+    margin: 3px 0;
+  }
+
+  &__information {
+    div {
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+
+  &__social {
+    display: flex;
+    justify-content: space-between;
+    margin-left: auto;
+
+    padding: 27px;
+
+    div {
+      margin: 0 9px;
+
+      img {
+        width: 20px;
+        height: 20px;
+      }
+    }
+  }
 }
 </style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="index">
+  <div>
     <section class="main">
       <div class="center">
         <h2 class="lead">Wij zijn</h2>
@@ -54,89 +54,81 @@ import { Component, Vue } from "vue-property-decorator";
 export default class Index extends Vue {}
 </script>
 <style lang="scss">
-.index {
+section.main {
   width: 100%;
   height: 100vh;
+  background: linear-gradient(rgba(102, 50, 101, 0.8), rgba(102, 50, 101, 0.8)),
+    url("../assets/images/background.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
 
-  section.main {
-    width: 100%;
-    height: 100%;
-    background: linear-gradient(
-        rgba(102, 50, 101, 0.8),
-        rgba(102, 50, 101, 0.8)
-      ),
-      url("../assets/images/background.jpg");
-    background-size: cover;
-    background-repeat: no-repeat;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  .center {
+    text-align: center;
+    font-family: Poppins, sans-serif;
 
-    .center {
-      text-align: center;
-      font-family: Poppins, sans-serif;
+    .lead {
+      font-size: 28px;
+      color: #ffffff;
+      font-weight: 400;
+    }
 
-      .lead {
-        font-size: 28px;
-        color: #ffffff;
-        font-weight: 400;
-      }
+    .title {
+      font-family: "Portico Rounded", sans-serif;
+      font-size: 66px;
+      line-height: 77px;
+      color: #ffffff;
+      margin-top: 40px;
+      margin-bottom: 10px;
+      font-weight: 400;
+    }
 
-      .title {
-        font-family: "Portico Rounded", sans-serif;
-        font-size: 66px;
-        line-height: 77px;
-        color: #ffffff;
-        margin-top: 40px;
-        margin-bottom: 10px;
-        font-weight: 400;
-      }
+    .notice {
+      font-size: 18px;
+      color: #ffffff;
+      font-weight: 400;
+    }
 
-      .notice {
-        font-size: 18px;
-        color: #ffffff;
-        font-weight: 400;
-      }
+    .click-to-action {
+      width: 160px;
 
-      .click-to-action {
-        width: 160px;
+      margin-top: 50px;
+      margin-left: auto;
+      margin-right: auto;
 
-        margin-top: 50px;
-        margin-left: auto;
-        margin-right: auto;
+      background: #663265;
+      border-radius: 2px;
 
-        background: #663265;
-        border-radius: 2px;
+      padding: 7px 26px;
 
-        padding: 7px 26px;
+      display: block;
 
-        display: block;
+      font-weight: bold;
+      font-size: 18px;
 
-        font-weight: bold;
-        font-size: 18px;
+      color: #ffffff;
 
-        color: #ffffff;
-
-        box-shadow: 2px 2px 6px 0px rgba(0, 0, 0, 0.4);
-        cursor: pointer;
-      }
+      box-shadow: 2px 2px 6px 0px rgba(0, 0, 0, 0.4);
+      cursor: pointer;
     }
   }
+}
 
-  section.about {
-    padding-top: 100px;
-    .container {
-      flex-direction: column;
-    }
+section.about {
+  padding-top: 100px;
+  .container {
+    flex-direction: column;
+  }
 
-    p {
-      margin: 15px 0;
-    }
+  p {
+    margin: 15px 0;
+  }
 
-    ul {
-      padding-left: 16px;
-    }
+  ul {
+    padding-left: 16px;
   }
 }
 </style>
