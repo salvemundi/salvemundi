f5a16c0aa4612a99e22c30cc21c06ba5a2b6e502
INeedSugar <bastiankpn7800@gmail.com>
2019-09-08 12:49:44 +0200

    Small changes to buton

--- src/components/basic/SaMuButton.vue
+++ src/components/basic/SaMuButton.vue
@@ -1,8 +1,8 @@
 <template>
     <router-link v-if="to" :to="to">
-        <button :class="['SaMuButton', size, className]">{{text}}</button>
+        <button :class="['SaMuButton', size === '' ? 'default' : size, className]">{{text}}</button>
     </router-link>
-    <button v-else :class="['SaMuButton', size, className]">{{text}}</button>
+    <button v-else :class="['SaMuButton', size === '' ? 'default' : size, className]">{{text}}</button>
 </template>
 <script>
 import { Component, Vue } from "vue-property-decorator";
@@ -17,13 +17,16 @@ export default class SaMuButton extends Vue {}
     border-radius: 2px;
     background-color: #663265;
     color: #fff;
-    padding: 1em 1.5em;
     border: none;
     font-family: Poppins, sans-serif;
     font-weight: 700;
     box-shadow: 2px 2px 6px 0px #000;
     cursor: pointer;
     transform: translateY(20%);
+
+    &.default {
+        padding: 1em 1.5em;
+    }
 
     &.small {
         padding: 1em;
