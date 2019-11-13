d6b82b55d7d19444192b287bc5f592e1bd9371b2
Niek <ik@niekvangogh.nl>
2019-11-13 13:58:20 +0100

    added store

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,13 @@ import router from './router';
 import store from './store';
 import Toasted from 'vue-toasted';
 import { LayoutPlugin, NavbarPlugin, ButtonPlugin } from 'bootstrap-vue';
-
 import VueI18n from 'vue-i18n';
 import VueCookies from 'vue-cookies';
+import Vuex from 'vuex';
 
 Vue.config.productionTip = false;
 
+Vue.use(Vuex)
 Vue.use(Toasted);
 Vue.use(LayoutPlugin);
 Vue.use(ButtonPlugin);
