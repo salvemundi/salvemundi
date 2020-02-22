b84dcf9fb977481d67b9068ca9ac80ce86fa8b55
Bastiaan Verhaar <verhaar.bastiaan@gmail.com>
2020-02-22 09:40:12 +0100

    Hotfix

--- api.salvemundi.nl/src/services/confirmation/mock.confirmation.service.ts
+++ api.salvemundi.nl/src/services/confirmation/mock.confirmation.service.ts
@@ -1,8 +1,14 @@
 import { IConfirmationService } from './iconfirmation.service';
 import { Confirmation } from '../../entities/core/confirmation.entity';
 import { User } from '../../entities/core/user.entity';
+import { BaseEntity } from 'typeorm';
 
 export class MockConfirmationService implements IConfirmationService {
+    delete<T extends BaseEntity>(entity: T): Promise<T> {
+        return new Promise<T>((resolve) => {
+            resolve(null);
+        });
+    }
     create(user: User): Promise<Confirmation> {
         return new Promise<Confirmation>((resolve) => {
             resolve(null);
