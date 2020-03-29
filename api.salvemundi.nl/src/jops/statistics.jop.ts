0937aedbb156f8239be499dbf877f6a109a98bb1
Bastiaan Verhaar <3987804+bastiaanv@users.noreply.github.com>
2020-03-29 21:18:03 +0200

    Hotfix

--- api.salvemundi.nl/src/jops/statistics.jop.ts
+++ api.salvemundi.nl/src/jops/statistics.jop.ts
@@ -10,8 +10,8 @@ export class StatisticsJob {
     ) {}
 
     @Cron('0 0 0 * * *')
-    addEmptyPageView() {
-        if (!this.statisticRepository.getPageViewToday()) {
+    async addEmptyPageView() {
+        if (!await this.statisticRepository.getPageViewToday()) {
             const today = new Date();
             today.setHours(0, 0, 0, 0);
 
